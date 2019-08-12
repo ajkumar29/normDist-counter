6350768860f89e3c5fb5ffbe63d95d5cf3177555
AJ Kumar <ajanthan29894@gmail.com>
2019-08-12 11:17:11 +0100

    Implemented mock counter using normal dis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  randn_bm(min, max, skew) {
+    var u = 0,
+      v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) num = this.randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
+    return num;
+  }
+
+  componentDidMount() {
+    var rand = this.randn_bm(0, 12000, 1);
+    setTimeout(this.increment, rand);
+    console.log(rand);
+  }
+
+  componentDidUpdate() {
+    var rand = this.randn_bm(0, 12000, 1);
+    setTimeout(this.increment, rand);
+    console.log(rand);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <p className='Counter-text'>{this.state.count}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
